23ec04b308341e83c1ca442519dae8c8abb671e6
europelee <ouzhou.li@quantil.com>
2017-06-11 23:04:44 +0800

    up

--- jenkins_ci_demo/jenkins_ci_demo.py
+++ jenkins_ci_demo/jenkins_ci_demo.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+
+class DemoAdd:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+
 def main():
     print "demo"
     print "trigger"
+    demo = DemoAdd()
+    print demo.add(3, 4)
+
 
 if __name__ == "__main__":
     main()
---
+++ test/test_jenkins_ci_demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import pytest
+from jenkins_ci_demo.jenkins_ci_demo import DemoAdd
+
+
+class TestDemoAdd(object):
+    @pytest.mark.parametrize("x, y, expected", [
+        (3, 4, 7),
+        (4, 5, 19)
+    ])
+    def test_add(self, x, y, expected):
+        assert DemoAdd.add(x, y) == expected
