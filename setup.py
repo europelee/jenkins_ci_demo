ebe09df4c80a6286395b5f3ef4e6265199490e8f
europelee <ouzhou.li@quantil.com>
2017-06-11 22:11:03 +0800

    up

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from jenkins_ci_demo import __version__
+from setuptools import setup
+# from distutils.core import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read()
+f.close()
+
+setup(name='jenkins_ci_demo',
+      version=__version__,
+      description='jenksin_ci_demo',
+      long_description=long_description,
+      author='europelee',
+      author_email='europe.lee@gmail.com',
+      packages=['jenkins_ci_demo'],
+      install_requires=[
+          "coverage==4.3.1",
+          "pytest==3.0.5",
+          "pytest-cov==2.4.0",
+          "pytest-html==1.13.0"
+      ]
+      )
