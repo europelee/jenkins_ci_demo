6fd24248f79f03c81f0df50b2b4186087c621c20
europelee <ouzhou.li@quantil.com>
2017-06-12 11:26:06 +0800

    up

--- test/test_jenkins_ci_demo.py
+++ test/test_jenkins_ci_demo.py
@@ -7,7 +7,7 @@ from jenkins_ci_demo.jenkins_ci_demo import DemoAdd
 class TestDemoAdd(object):
     @pytest.mark.parametrize("x, y, expected", [
         (3, 4, 7),
-        (4, 5, 19)
+        (4, 5, 9)
     ])
     def test_add(self, x, y, expected):
         assert DemoAdd.add(x, y) == expected
