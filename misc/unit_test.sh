7711a7a933776a168530e881befb9e2a81a5010f
europelee <europe.lee@gmail.com>
2017-06-20 18:16:05 +0800

    refactor

---
+++ misc/unit_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Absolute path to this script
+SCRIPT=$(readlink -f $0)
+#Absolute path this script is in
+SCRIPTPATH=$(dirname $SCRIPT)
+
+cd $SCRIPTPATH
+py.test $SCRIPTPATH/test/test_jenkins_ci_demo.py --html=$SCRIPTPATH/test_report/test_result.html
